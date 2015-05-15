5741a7c35813eb05580284e7d6f4aea55cfbfc8b
Miroslav Kouril <miroslav.kouril@gradweb.co.uk>
2015-05-15 09:00:39 +0100

    Adding UITests examples

---
+++ JustEatDemo.UITests/Activities/InsetLocationActivityTestGPSLocation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+using Xamarin.UITest.Android;
+using Xamarin.UITest;
+
+namespace JustEatDemo.UITests
+{
+	[TestFixture]
+	public class InsetLocationActivityTestGPSLocation
+	{
+		AndroidApp app;
+
+		[SetUp]
+		public void BeforeEachTest()
+		{
+			app = ConfigureApp.Android.StartApp();
+			app.Device.SetLocation(51.36822357, -1.32466716);
+		}
+
+		[Test]
+		public void DisplayReplConsole()
+		{
+			app.Repl();
+		}
+
+		[Test]
+		public void InsertLocationScreenVisible()
+		{
+			app.WaitForElement(c => c.Marked("subtitle"));
+		}
+
+		[Test]
+		public void HappyPathFunctionalityManualPostcode()
+		{
+			app.Tap(c => c.Marked("postcode_from_gps"));
+			app.WaitForElement(c => c.Marked("postcode").Text("RG20 9DA"), "Postcode from GPS timeout", TimeSpan.FromMinutes(1));
+		}
+	}
+}
+
---
+++ JustEatDemo.UITests/Activities/InsetLocationActivityTestManualInsert.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+using Xamarin.UITest.Android;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace JustEatDemo.UITests
+{
+	[TestFixture]
+	public class InsetLocationActivityTestManualInsert
+	{
+		AndroidApp app;
+
+		[SetUp]
+		public void BeforeEachTest()
+		{
+			app = ConfigureApp.Android.StartApp();
+		}
+
+		[Test]
+		public void DisplayReplConsole()
+		{
+			app.Repl();
+		}
+
+		[Test]
+		public void InsertLocationScreenVisible()
+		{
+			app.WaitForElement(c => c.Marked("subtitle"));
+		}
+
+		[Test]
+		public void HappyPathFunctionalityManualPostcode()
+		{
+			app.EnterText(c => c.Marked("postcode"), "RG20");
+			app.Tap(c => c.Marked("find_takeaways"));
+			app.WaitForElement(c => c.Marked("list"));
+		}
+	}
+}
+
